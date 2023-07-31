d1dd4cea6bf1f7803033cb58872bc37d7cb7e10d
Typicek <carotkacz333@seznam.cz>
2023-07-31 11:41:04 +0200

    Initial commit

---
+++ src/main/java/com/project/chilliwebapp_backend/ChilliWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.chilliwebapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilliWebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChilliWebAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/chilliwebapp_backend/ChilliWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chilliwebapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChilliWebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
